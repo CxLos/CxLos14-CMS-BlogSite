1fcb9bb974e20160f223a909378907059b8f63cf
cxlos <bautistac_29@outlook.com>
2023-02-21 22:02:21 -0600

    random commit

--- controllers/api/blogsuRoutes.js
+++ controllers/api/blogsuRoutes.js
@@ -6,6 +6,7 @@ const withAuth = require('../../utils/auth');
 router.put('/:id', withAuth, async (req, res) => {
 
     try {
+      console.log('yay updates')
       const blogData = await Blog.update(req.body, {
         where: {
           id: req.params.id,
@@ -17,7 +18,7 @@ router.put('/:id', withAuth, async (req, res) => {
         res.status(404).json({ message: 'No blog found with this id!' });
         return;
       }
-  
+      
       res.status(200).json(blogData);
     } catch (err) {
       res.status(500).json(err);
--- public/js/upBlog.js
+++ public/js/upBlog.js
@@ -1,44 +1,49 @@
-const updateBlog = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+// const updateBlog = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/blogsu/${id}`, {
-        method: 'PUT',
-      });
-  
-      if (response.ok) {
-        document.location.replace(`/api/blogs/${result.id}`);
-      } else {
-        alert('Failed to update blog');
-      }
-    }
-    console.log('yay updates!')
-  };
-
-//   const updateBlog = async (event) => {
-//     event.preventDefault();
-  
-//     const title = document.querySelector('#blog-title').value.trim();
-//     const body = document.querySelector('#blog-body').value.trim();
-  
-//     if (title && body) {
 //       const response = await fetch(`/api/blogsu/${id}`, {
 //         method: 'PUT',
 //         body: JSON.stringify({ title, body }),
 //         headers: {
-//           'Content-Type': 'application/json',
+//             'Content-Type': 'application/json',
 //         },
 //       });
   
 //       if (response.ok) {
-//         let result = await response.json();
-//         document.location.replace(`/blogs/${result.id}`);
+//         document.location.replace(`/api/blogs/${result.id}`);
 //         // document.location.replace(`/profile`);
 //       } else {
-//         alert('Failed to post blog');
+//         alert('Failed to update blog');
 //       }
 //     }
+//     console.log('')
 //   };
+
+  const updateBlog = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#blog-title').value.trim();
+    const body = document.querySelector('#blog-body').value.trim();
+  
+    if (title && body) {
+      const response = await fetch(`/api/blogsu/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, body }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        let result = await response.json();
+        document.location.replace(`api/blogs/${result.id}`);
+        // document.location.replace(`/profile`);
+      } else {
+        alert('Failed to post blog');
+      }
+    }
+  };
 
   document.querySelector('.blog-up').addEventListener('click', updateBlog);
 //   document.querySelector('#new-blog-form').addEventListener('submit', updateBlog);
